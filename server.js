e4bde38addac4ebfe259289dd65aa2a3f9750483
Frank Acquah <acquahfrank25@gmail.com>
2023-06-19 13:41:14 +0000

    server configured

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 import express from 'express';
 import {connect} from 'mongoose';
 import router from './src/routes/task.js';
+import { DATABASE_CONNECTION_STRING } from './src/config/database.js';
+import { PORT } from './src/config/application.js';
 
 
 const app = express();
+
+//the function connects to a database and starts the server
+async function startServer(){
+    try{
+        await connect(DATABASE_CONNECTION_STRING,{useNewUrlParser:true,useUnifiedTopology:true,family:4});
+        app.listen(PORT,()=>console.log(`serer running on ${PORT}`));
+
+    }catch(err){
+        console.log('Database connection failed', err);
+        
+    }
+}
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 app.use(router);
+
+startServer();
---
+++ src/config/application.js
@@ -0,0 +1,4 @@
+import dotenv from 'dotenv';
+dotenv.config({override:true});
+
+export const PORT = process.env.PORT;
